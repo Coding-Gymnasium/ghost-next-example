54de57232797d04d7aae95e5ee75b8c37ed52495
Nico Rithner <nicorithner@gmail.com>
2024-06-27 11:11:47 -0600

    Add postSlug

--- src/app/components/BlogPost.jsx
+++ src/app/components/BlogPost.jsx
@@ -1,13 +1,21 @@
+import Link from "next/link";
+
 const blogStyles = {
   border: "1px solid white",
   padding: "20px",
   margin: "20px",
 };
-export default function BlogPost({ title, plainText, excerpt = null }) {
+export default function BlogPost({
+  title,
+  plainText,
+  postSlug,
+  excerpt = null,
+}) {
   return (
     <div style={blogStyles}>
       <h3>{title}</h3>
       <p>{excerpt ? excerpt : plainText}</p>
+      {excerpt && <Link href={`/posts/${postSlug}`}>View Posts</Link>}
     </div>
   );
 }
